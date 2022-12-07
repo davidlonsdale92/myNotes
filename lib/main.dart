44505a5b7571f9ebd8f1a95ceeca63435671bc4a
David Lonsdale <davidlonsdale_1992@hotmail.com>
2022-12-07 13:25:44 +0000

    Bug Fix
    Homepage removed from main.dart in error and added to Verify view. Reverted this.

--- lib/main.dart
+++ lib/main.dart
@@ -34,3 +34,30 @@ class MyApp extends StatelessWidget {
     );
   }
 }
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return FutureBuilder(
+      future: Firebase.initializeApp(
+        options: DefaultFirebaseOptions.currentPlatform,
+      ),
+      builder: (context, snapshot) {
+        switch (snapshot.connectionState) {
+          case ConnectionState.done:
+            // final user = FirebaseAuth.instance.currentUser;
+            // if (user?.emailVerified ?? false) {
+            //   return const Text('Done');
+            // } else {
+            //   return const VerifyEmailView();
+            // }
+            return const LoginView();
+          default:
+            return const CircularProgressIndicator();
+        }
+      },
+    );
+  }
+}
--- lib/views/verify_email_view.dart
+++ lib/views/verify_email_view.dart
@@ -5,33 +5,6 @@ import 'package:mynotes/views/login_view.dart';
 import 'package:mynotes/views/register_view.dart';
 import '../firebase_options.dart';
 
-class HomePage extends StatelessWidget {
-  const HomePage({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return FutureBuilder(
-      future: Firebase.initializeApp(
-        options: DefaultFirebaseOptions.currentPlatform,
-      ),
-      builder: (context, snapshot) {
-        switch (snapshot.connectionState) {
-          case ConnectionState.done:
-            // final user = FirebaseAuth.instance.currentUser;
-            // if (user?.emailVerified ?? false) {
-            //   return const Text('Done');
-            // } else {
-            //   return const VerifyEmailView();
-            // }
-            return const LoginView();
-          default:
-            return const Circular;
-        }
-      },
-    );
-  }
-}
-
 class VerifyEmailView extends StatefulWidget {
   const VerifyEmailView({super.key});
 
