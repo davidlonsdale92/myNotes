68beae74690ef3d135f46d27e1d042fcedfe4557
David Lonsdale <davidlonsdale_1992@hotmail.com>
2023-09-16 23:13:17 +0100

    Updated .gitignore

--- lib/views/login_view.dart
+++ lib/views/login_view.dart
@@ -92,3 +92,26 @@ class _LoginViewState extends State<LoginView> {
     );
   }
 }
+
+Future<void> showErrorDialog(
+  BuildContext context,
+  String text,
+) {
+  return showDialog(
+    context: context,
+    builder: (context) {
+      return AlertDialog(
+        title: const Text('An error occured.'),
+        content: Text(text),
+        actions: [
+          TextButton(
+            onPressed: () => {
+              Navigator.of(context).pop(),
+            },
+            child: const Text('OK'),
+          ),
+        ],
+      );
+    },
+  );
+}
